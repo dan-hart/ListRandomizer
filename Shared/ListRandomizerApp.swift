90c6bfd3a1568b834ecda8683eb3632250911d50
Dan Hart <dhart94@gmail.com>
2021-02-01 11:04:24 -0600

    Initial Commit

---
+++ Shared/ListRandomizerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ListRandomizerApp.swift
+//  Shared
+//
+//  Created by Dan Hart on 2/1/21.
+//
+
+import SwiftUI
+
+@main
+struct ListRandomizerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
