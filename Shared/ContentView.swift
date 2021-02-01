2aff7b4ca9af24ae286874a2be4aca84e48c813e
Dan Hart <dhart94@gmail.com>
2021-02-01 11:38:49 -0600

    Initial Implementation

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -8,14 +8,51 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var items: [Item] = []
+    @State private var currentItem: String = ""
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
-    }
-}
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+        VStack {
+            Text("List Randomizer")
+                .font(.largeTitle)
+                .padding()
+            
+            HStack {
+                TextField("Item", text: $currentItem)
+                Spacer()
+                Button(action: {
+                    items.append(Item(name: currentItem))
+                    currentItem = ""
+                }, label: {
+                    Text("Add")
+                })
+            }.padding()
+            
+            Divider()
+            
+            List {
+                ForEach(items) { (item) in
+                    Text(item.name)
+                }
+            }
+            
+            Button(action: {
+                items.shuffle()
+            }, label: {
+                Text("Shuffle")
+                    .font(.title)
+            })
+            
+            Spacer()
+            
+            Button(action: {
+                items = []
+            }, label: {
+                Text("Clear")
+                    .font(.subheadline)
+            })
+            
+            Spacer()
+        }
     }
 }
---
+++ Shared/Item.swift
@@ -0,0 +1,13 @@
+//
+//  Item.swift
+//  ListRandomizer
+//
+//  Created by Dan Hart on 2/1/21.
+//
+
+import Foundation
+
+struct Item: Identifiable {
+    var id = UUID()
+    var name: String
+}
